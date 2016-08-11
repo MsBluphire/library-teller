d73e776bf37e6e9a20fce96dddef81d623a919ee
Denise Williams <williamsde@outlook.com>
2016-08-11 13:28:27 -0400

    Updated Book, Magazine, and DVD class

--- Library/Book.cs
+++ Library/Book.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Library
 {
-    class Book
+    class Book : Media
     {
+        public override int RentalLength
+        {
+            get
+            {
+                return 14;
+            }
+
+            set
+            {
+                
+            }
+        }
+
+        public override void PrintMediaDetails()
+        {
+            Console.WriteLine("Book {0} {1}", Title, Length);
+            Console.WriteLine("Rented on: {0}", RentedDate);
+            Console.WriteLine("Due on: {0}", ReturnDueDate());
+        }
+
+        public override DateTime ReturnDueDate()
+        {
+            return RentedDate.AddDays(RentalLength);
+        }
     }
 }
--- Library/Magazine.cs
+++ Library/Magazine.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Library
 {
-    class Magazine
+    class Magazine : Media
     {
+        public override int RentalLength
+        {
+            get
+            {
+                return 3;
+            }
+
+            set
+            {
+               
+            }
+        }
+
+        public override void PrintMediaDetails()
+        {
+            Console.WriteLine("Magazine {0} ({1})", Title, Length);
+            Console.WriteLine("Rented on: {0}", RentedDate);
+            Console.WriteLine("Due on: {0}", ReturnDueDate());
+        }
+
+        public override DateTime ReturnDueDate()
+        {
+            return RentedDate.AddDays(RentalLength);
+        }
     }
 }
